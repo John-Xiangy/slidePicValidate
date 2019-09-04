0a842b2a25ce5f14e53a66f587b64f50fe85552e
Antinio-X <534572042@qq.com>
2019-09-04 11:14:37 +0800

    init

---
+++ tncode/tncode.php
@@ -0,0 +1,9 @@
+<?php
+/*! tncode 1.2 author:weiyingbin email:277612909@qq.com
+*/
+error_reporting(0);
+require_once dirname(__FILE__).'/TnCode.class.php';
+$tn  = new TnCode();
+$tn->make();
+
+?>
